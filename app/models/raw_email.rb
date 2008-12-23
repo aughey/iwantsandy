1c282e0081bce1fac90d45b9175a580ebc9333ca
John Aughey <jha@aughey.com>
2008-12-23 16:35:28 -0600

    Added public and private methods to parse the mail using TMail

--- app/models/raw_email.rb
+++ app/models/raw_email.rb
@@ -1,2 +1,15 @@
 class RawEmail < ActiveRecord::Base
+  def to
+    tmail.to
+  end
+
+  def body
+    tmail.body
+  end
+
+  protected
+
+  def tmail
+    @tmail ||= TMail::Mail.parse(self.rfc822)
+  end
 end
